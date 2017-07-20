df5a771700221c9e9e5109fa2df9fc482000b058
vks4303 <vks4303@gmail.com>
2017-07-20 22:32:25 +0530

    Initial Commit

---
+++ Object Detection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Object Detection
+//
+//  Created by Vaibhav Kumar Singh on 20/07/17.
+//  Copyright © 2017 Vaibhav Kumar Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
